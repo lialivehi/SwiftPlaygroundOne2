35d5b07ca24acd73636dcb062e587e1e1cc0d306
lialivehi <111819496+lialivehi@users.noreply.github.com>
2022-09-01 22:39:32 -1000

    Fimished Swift Playground one Assignment

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,3 +2,14 @@ import UIKit
 
 var str = "Hello, playground"
 //finally!!
+
+var myFirstVariable: String
+myFirstVariable = "I've created my first variable"
+
+// Doing something with two strings
+var mySecondVariable = "and look I added two strings together"
+
+
+
+var myThirdVariable = myFirstVariable + mySecondVariable
+print(myThirdVariable)
